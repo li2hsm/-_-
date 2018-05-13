2b6ad12697ab50dabcd4ebfd47090303b2fac55a
tom <244134860@qq.com>
2018-05-13 22:39:12 +0800

    广播使用

---
+++ Twenty/app/src/main/java/com/sw0039/justdoit/fours_zujian/BroadcastReceivers/AddressListBroadcastReceiver.java
@@ -0,0 +1,16 @@
+package com.sw0039.justdoit.fours_zujian.BroadcastReceivers;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+/**
+ * 监听通讯录变化的广播
+ */
+public class AddressListBroadcastReceiver extends BroadcastReceiver {
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        //弹前台通知
+
+    }
+}
---
+++ Twenty/app/src/main/java/com/sw0039/justdoit/fours_zujian/BroadcastReceivers/TestAdressListActivity.java
@@ -0,0 +1,18 @@
+package com.sw0039.justdoit.fours_zujian.BroadcastReceivers;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.sw0039.justdoit.R;
+
+/**
+ * 测试监听通讯录变化
+ */
+public class TestAdressListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_test_adress_list);
+    }
+}
